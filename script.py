ce199e52611d5b2b2ef1e7f8d56b42ea7122f92a
zhangtj1996 <zhangtj1996@users.noreply.github.com>
2021-08-24 10:44:28 +0800

    Update script.py
    add more comments

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ start = time.time()
 ###############################################################
 ## Split and distribute data to all nodes
 if rank == 0:
-#     df = pd.read_csv("mimic_data/0.5missingdata.csv",index_col=0,chunksize=2000)
+    # split data at master
     df=[4,4,4,4,4]
     dflist=[]
     for i in df:
@@ -21,6 +21,8 @@ if rank == 0:
     print("I'm master, I'm distributing data")
 else:
     dflist=None
+    
+# distribute data. Note that the rank0 node is both the master and slave. Rand0 plays different roles in different time intervals.    
 data = comm.scatter(dflist,root=0)
 print("My rank:",rank, "My data:", data,"Time:", time.time()-start)
 comm.barrier() 
@@ -32,6 +34,8 @@ comm.barrier()
 
 X=np.array(rank)+data
 for i in range(10):
+    
+    # test if barrier works one can remove this
     if rank == 1: 
         time.sleep(0.1)
     if rank == 2: 
@@ -40,22 +44,21 @@ for i in range(10):
         time.sleep(0.3)
     
     comm.barrier() 
+    
+    # sent data to master
     data1 = comm.gather(X, root=0)
+    
+    comm.barrier()
+    
     print(data1)
     if rank == 0:
         print("rank = %s " %rank + "...receiving data to other process")
+        # processing the received data at the master (which is a list)
         X=sum(data1)/5
     else:
         X=None
+        
+    # broadcast the processed data to all nodes    
     X=comm.bcast(X, root=0)
     X=rank+X
     print("epoch:",i,"My rank:",rank,"data",X,"Time:", time.time()-start)    
-#     for i in range(1, size):
-#         data[i] = (i+1)**2
-#         value = data[i]
-#         print(" process %s receiving %s from process %s" % (rank , value , i))
-
-
-
-
-
